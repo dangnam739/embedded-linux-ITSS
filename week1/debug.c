2a69245769b4577026ed2f504f555e6638a8f9cf
dangnam739 <kieudangnam99@gmail.com>
2021-02-23 19:33:08 +0700

    add week1 exercise

---
+++ week1/debug.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int foo(int i);
+void var(int val);
+
+int main(void)
+{
+    foo(5);
+    return (0);
+}
+
+int foo(int i)
+{
+    var(i);
+    return (0);
+}
+
+void var(int val)
+{
+    int i = 0;
+    for (; val > 0; val--)
+    {
+        i += val;
+    }
+}
